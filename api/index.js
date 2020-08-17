ec2ffd4e991120c3c159d31734e9564a85100713
NitinK <nitin.khanchandani@inloop.com.au>
2020-08-18 08:17:25 +1000

    Initial Commit

---
+++ api/index.js
@@ -0,0 +1,14 @@
+const axios = require('axios')
+
+module.exports = (req, res) => {
+    const randomNumber = Math.random() * (1600 - 1) + 1;
+    const value = Math.floor(randomNumber);
+    const url = 'https://type.fit/api/quotes'
+
+    axios
+        .get(url)
+        .then((resp) => {            
+            res.send(resp.data[value]);
+        })
+
+}
